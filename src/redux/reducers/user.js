b5799728ba13c296e85b65c360ed94ee4cd213fd
wladyrjusta <wladyr.justa@gmail.com>
2023-03-09 18:19:56 -0300

    Criados reducers, rootReducer, Store. Store fornecida ao componente principal App através do Provider.

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,7 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  email: '',
+};
+
+const user = (state = INITIAL_STATE) => state;
+
+export default user;
--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,1 +1,10 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+  editor: false,
+  idToEdit: 0,
+};
+
+const wallet = (state = INITIAL_STATE) => state;
+
+export default wallet;
