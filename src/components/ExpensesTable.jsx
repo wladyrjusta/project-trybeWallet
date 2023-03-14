f058bb81d6f5ebc848761c73c77785fe7a020d7c
wladyrjusta <wladyr.justa@gmail.com>
2023-03-14 20:43:21 -0300

    Requisito 7.

--- src/components/ExpensesTable.jsx
+++ src/components/ExpensesTable.jsx
@@ -1,35 +1,68 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class ExpensesTable extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <div>
         <table>
           <caption>Tabela de Gastos</caption>
-          <tr>
-            <th scope="col">Descrição</th>
-            <th scope="col">Tag</th>
-            <th scope="col">Método de pagamento</th>
-            <th scope="col">Valor</th>
-            <th scope="col">Moeda</th>
-            <th scope="col">Câmbio utilizado</th>
-            <th scope="col">Valor convertido</th>
-            <th scope="col">Moeda de conversão</th>
-            <th scope="col">Editar/Excluir</th>
-          </tr>
-          <td>Conta de água</td>
-          <td>Contas</td>
-          <td>Contas</td>
-          <td>Contas</td>
-          <td>Contas</td>
-          <td>Contas</td>
-          <td>Contas</td>
-          <td>Contas</td>
-          <td>Contas</td>
+          <thead>
+            <tr>
+              <th scope="col">Descrição</th>
+              <th scope="col">Tag</th>
+              <th scope="col">Método de pagamento</th>
+              <th scope="col">Valor</th>
+              <th scope="col">Moeda</th>
+              <th scope="col">Câmbio utilizado</th>
+              <th scope="col">Valor convertido</th>
+              <th scope="col">Moeda de conversão</th>
+              <th scope="col">Editar/Excluir</th>
+            </tr>
+          </thead>
+          <tbody>
+            { expenses.map((expense) => (
+              <tr key={ expense.id }>
+                <td>{`${expense.description}`}</td>
+                <td>{`${expense.tag}`}</td>
+                <td>{`${expense.method}`}</td>
+                <td>{`${Number(expense.value).toFixed(2)}`}</td>
+                <td>{`${expense.exchangeRates[expense.currency].name}`}</td>
+                <td>
+                  {`${Number(expense.exchangeRates[expense.currency].ask).toFixed(2)}`}
+                </td>
+                <td>
+                  {
+                    `${Number(expense.exchangeRates[expense.currency].ask * expense.value)
+                      .toFixed(2)}`
+                  }
+                </td>
+                <td>Real</td>
+                <td>X</td>
+              </tr>)) }
+          </tbody>
         </table>
       </div>
     );
   }
 }
 
-export default ExpensesTable;
+const mapStateToProps = (state) => ({
+  ...state.user,
+  ...state.wallet,
+});
+
+ExpensesTable.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+    value: PropTypes.string,
+    description: PropTypes.string,
+    currency: PropTypes.string,
+    method: PropTypes.string,
+    tag: PropTypes.string,
+  })).isRequired,
+};
+
+export default connect(mapStateToProps)(ExpensesTable);
